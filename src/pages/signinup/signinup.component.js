af1aea42ddceba3d1c7b5fad89e9ab94d5fdd6c6
Bogdan Laban <auroraborealismcc@gmail.com>
2021-02-10 22:52:44 +0100

    sign up with firebase

--- src/pages/signinup/signinup.component.js
+++ src/pages/signinup/signinup.component.js
@@ -4,10 +4,12 @@ import React from 'react'
 import './signinup.styles.scss'
 
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 const SignInUp = () => (
   <div className="signinup">
     <SignIn />
+    <SignUp />
   </div>
 )
 
